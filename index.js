f67c25a93b07a8e65cce82bde2e069d61ec14655
Artem Rudenko <rtomrud@gmail.com>
2018-11-17 12:39:00 +0100

    Remove RegExp named capture groups

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const durationRegExp = /P((?<years>\d+(\.\d+)?)Y)?((?<months>\d+(\.\d+)?)M)?((?<weeks>\d+(\.\d+)?)W)?((?<days>\d+(\.\d+)?)D)?(T((?<hours>\d+(\.\d+)?)H)?((?<minutes>\d+(\.\d+)?)M)?((?<seconds>\d+(\.\d+)?)S)?)?/;
+const durationRegExp = /P((\d+(\.\d+)?)Y)?((\d+(\.\d+)?)M)?((\d+(\.\d+)?)W)?((\d+(\.\d+)?)D)?(T((\d+(\.\d+)?)H)?((\d+(\.\d+)?)M)?((\d+(\.\d+)?)S)?)?/u;
 
 /**
  * Converts the given ISO 8601 `duration` to seconds. A year ("Y") is regarded
@@ -10,18 +10,30 @@ export default function(duration = "") {
     return 0;
   }
 
-  const match = durationRegExp.exec(duration) || {};
-  const {
-    groups: {
-      years = 0,
-      months = 0,
-      weeks = 0,
-      days = 0,
-      hours = 0,
-      minutes = 0,
-      seconds = 0
-    } = {}
-  } = match;
+  const [
+    ,
+    ,
+    years = 0,
+    ,
+    ,
+    months = 0,
+    ,
+    ,
+    weeks = 0,
+    ,
+    ,
+    days = 0,
+    ,
+    ,
+    ,
+    hours = 0,
+    ,
+    ,
+    minutes = 0,
+    ,
+    ,
+    seconds = 0
+  ] = durationRegExp.exec(duration) || [];
   return (
     years * 31536000 +
     months * 2592000 +
